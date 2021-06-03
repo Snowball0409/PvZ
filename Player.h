1a53f91b951787a6748f945cde412992eaac85a6
Snowball <kai890409@gmail.com>
2021-06-03 12:08:43 +0800

    Solve method name problem

--- Player.h
+++ Player.h
@@ -9,7 +9,7 @@ class Player
     public:
         Player();
         virtual ~Player();
-        bool Plant(const Plant plant);
+        bool Planting(const Plant plant);
         void Move(const int loc);
 
     protected:
