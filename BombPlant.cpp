eb174953df6a3ffdcde07ee65d4c795e1d41b74a
owancato <owancato@gmail.com>
2021-06-10 18:00:01 +0800

    0610

---
+++ BombPlant.cpp
@@ -0,0 +1,13 @@
+#include "Plant.h"
+#include <iostream>
+#include "BombPlant.h"
+
+
+
+BombPlant::BombPlant(){
+    damage_ = 50;
+}
+
+BombPlant::~BombPlant(){
+
+}
---
+++ HealPlant.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Plant.h"
+#include "HealPlant.h"
+
+HealPlant::HealPlant(){
+    healPoint_ = 20;
+}
+
+HealPlant::~HealPlant(){
+
+}
