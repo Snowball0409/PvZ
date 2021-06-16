a6f47bd20e29b4d41b7c3ef45191f53f09445190
Snowball <kai890409@gmail.com>
2021-06-17 04:21:42 +0800

    Update visit method

--- Land.cpp
+++ Land.cpp
@@ -2,7 +2,7 @@
 	
 Land::Land()
 {
-	
+	//create empty plant
 }
 	
 Land::~Land()
@@ -20,8 +20,19 @@ void Land::Planting(const Plant plant)
     plant_ = plant;
 }
 
+void Visit(Player &player)
+{
+    //if plant exist&has visit method, visit
+}
+
+void Visit(Zombie &zombie)
+{
+    //if plant exist, battle
+}
+
 std::ostream& Land::operator <<(std::ostream &os, const Land &land)
 {
+    //print plant info
     os << land.plant_;
     return os;
 }
