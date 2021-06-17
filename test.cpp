6bf7353a48b4307e3d60917c31167bc0b355a9a7
Snowball <kai890409@gmail.com>
2021-06-17 19:34:16 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -15,8 +15,11 @@ using namespace std;
 
 int main()
 {
+    //////////////////////////////////////////////////////////////////////////////////
     //create class
     cout << "Init class test..." << endl;
+    int landN = 8;
+    int zombieN = 3;
     Plant p0;
     BombPlant p1;
     CoinPlant p2;
@@ -25,16 +28,42 @@ int main()
 
     Player player;
     Zombie zombies[3];
-    Zombie zombie(0,5);
+    Zombie zombie(15,5);//don't change
     Land land;
-    Map map(8, player, 3, zombies);
-
-    //map show
-    cout << "Show data test..." << endl;
+    Map map(landN, player, zombieN, zombies);
+    //////////////////////////////////////////////////////////////////////////////////
+    //plant test
+    cout << "//////////////////////////////////////////////////////////////////////////////////" << endl;
+    cout << "Planting test..." << endl;
     try{
-        //map show
-        map.Show(); 
 
+    }catch(const exception e)
+    {
+        cerr << e.what() << endl;
+    }
+    //////////////////////////////////////////////////////////////////////////////////
+    //player test
+    cout << "//////////////////////////////////////////////////////////////////////////////////" << endl;
+    cout << "Player test..." << endl;
+    try{
+        //planting test
+        while(player.Planting(p0))cout << "Plant: " << p0.Name() << ", Last: " << player.Money() << endl;
+
+        //moving test
+        player.Move(1, landN);
+        cout << "Locate: " << player.Locate() << ", Except: 1" << endl;
+        player.Move(7, landN);
+        cout << "Locate: " << player.Locate() << ", Except: 0" << endl;
+
+    }catch(const exception e)
+    {
+        cerr << e.what() << endl;
+    }
+    //////////////////////////////////////////////////////////////////////////////////
+    //zombie test
+    cout << "//////////////////////////////////////////////////////////////////////////////////" << endl;
+    cout << "Zombie test..." << endl;
+    try{
         //zombie show
         for(size_t i = 0;i < 3;++ i)
         {
@@ -43,12 +72,49 @@ int main()
                 cout << '[' << i << ']' << zombies[i] << endl;
             }
         }
-        cout << zombie << endl;
+
+        //damaged
+        zombie.Damaged(10);
+        cout << "Zombie hp: " << zombie.HealthPoint() << ", Except: 5" << endl;
+
+        //attack
+        cout << "ATK: " << zombie.Damage() << ", Except: 5" << endl;
+
+        //isdie
+        zombie.Damaged(1000);
+        cout << "IsDie: " << boolalpha << zombie.IsDie() << ", Except: true" << endl; 
+
+        //move
+        zombie.Move(1, landN);
+        cout << "Locate: " << zombie.Locate() << ", Except: " << (zombie.Locate()+1)%landN << endl;
+        zombies[0].Move(7, landN);
+        cout << "Locate: " << zombie.Locate() << ", Except: " << (zombie.Locate()+7)%landN << endl;
+
+    }catch(const exception e)
+    {
+        cerr << e.what() << endl;
+    }
+    //////////////////////////////////////////////////////////////////////////////////
+    //map test
+    cout << "//////////////////////////////////////////////////////////////////////////////////" << endl;
+    cout << "Map test..." << endl;
+    try{
+        //map show
+        map.Show();
+
+        //planting
+        map.Planting(p1, 0);
+        map.Show();
+        cout << "Except: [0]{*    }" << p1.Name();
+
+        //Visit
+
+        //Update
 
     }catch(const exception& e)
     {
-        e.what();
+        cerr << e.what() << endl;
     }
-
+    //////////////////////////////////////////////////////////////////////////////////
     return 0;
 }
