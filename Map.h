810851c1bf24b9a2a81a2108542a1b6913c11ad8
Snowball <kai890409@gmail.com>
2021-06-17 11:57:16 +0800

    Create New Map branch

---
+++ Map.h
@@ -0,0 +1,33 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include "Land.h"
+#include "Player.h"
+#include "Zombie.h"
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+	
+class Map  
+{
+	private:
+		Land* lands_;
+		int playerLoc_;
+		int* zombieLoc_;
+
+	public:
+
+		Map(const int landN, const Player &player, const int zombieN, const Zombie *zombies);
+		~Map();
+
+		void Update(Player &player);
+		void Update(Zombie &zombie, const int index);
+		void Visit(Player &player);
+		void Visit(Zombie &zombie, const int index);
+		static const int Random();
+		void Show();
+		void Planting(const Plant &plant, const int index);
+
+};
+#endif
