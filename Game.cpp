1c64654159ff18b435a427182f420b7bc9ab03be
Allen5123 <allen23648858@yahoo.com>
2021-06-21 01:49:19 +0800

    Print Map & Zombie Ok

--- Game.cpp
+++ Game.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstdio>
 constexpr char Game::fileName[];
-Game::Game():success_{false}
+Game::Game():success_{false}, numOfZombie_{DEFAULT_ZOMBIE}, numOfLand_{DEFAULT_LAND}
 {
     std::ifstream inputFile(fileName, std::ios::in);
     if (!inputFile)
@@ -13,13 +14,53 @@ Game::Game():success_{false}
     }
     else
     {
-        std::string line;
-        while (getline(inputFile, line))
-        {
-            //std::cout << line;
-            /*Set plant's Attributes*/
+        for (size_t i = 0; i < NUM_OF_PLANT_TYPES; ++i) {
+            std::string line, plantType, plantName, plantCostStr;
+            int plantHp, plantCost;
+            getline(inputFile, line);
+            std::istringstream iss(line);
+            iss >> plantType >> plantName >> plantCostStr  >> plantHp;
+            plantCost = stoi(plantCostStr.substr(1));
+            printf("plantType:%s, plantName:%s plantCost:%d plantHp:%d ", plantType.c_str(), plantName.c_str(), plantCost, plantHp);
+            switch (plantType[0])
+            {
+                case 'C':
+                    int visit, reward;
+                    iss >> visit >> reward;
+                    printf("visit:%d reward:%d\n", visit, reward);
+                    plants_[COIN_PLANT] = new CoinPlant(plantCost, plantHp, reward, visit, plantName);
+                    break;
+                case 'S':
+                    int dmg;
+                    iss >> dmg;
+                    printf("dmg:%d\n", dmg);
+                    plants_[HORN_PLANT] = new HornPlant(plantCost, plantHp, dmg, plantName);
+                    break;
+                case 'B':
+                    printf("\n");
+                    plants_[BOMB_PLANT] = new BombPlant(plantCost, plantHp, plantHp, plantName);
+                    break;
+                case 'H':
+                    int health;
+                    iss >> health;
+                    printf("health:%d\n", health);
+                    plants_[HEAL_PLANT] = new HealPlant(plantCost, plantHp, health, plantName);
+                    break;
+                default:
+                    std::cout << "Unknown Plant Type " << plantType << std::endl;
+                    break;
+            }
         }
+        //waiting for plain virtual show()
+        // for (int i = 0; i < NUM_OF_PLANT_TYPES; ++i) {
+        //     std::cout << plants_[i] << "\n";
+        // }
         GameSetUp();
+        player_ = new Player(numOfLand_);
+        for (size_t i = 0; i < numOfZombie_; ++i) {
+            zombies_.push_back(new Zombie(numOfLand_));
+        }
+        map_ = new Map(numOfLand_, *player_, numOfZombie_, zombies_);
         success_ = true;
     }
     inputFile.close();
@@ -30,21 +71,20 @@ void Game::GameSetUp()
     << "------------------------------\n"
     << "|     Plants vs. Zombies     |\n"
     << "------------------------------\n";
-    int numLand = 8, numZombie = 3;
     std::string userInput;
     std::istringstream iss;
     std::cout << "Number of lands on the map (1-10, default: 8)...>";
     std::getline(std::cin, userInput);
     iss.str(userInput);
-    iss >> numLand;
+    iss >> numOfLand_;
     iss.clear();
     std::cout << "Number of zombies on the map (1-10, default: 3)...>";
     std::getline(std::cin, userInput);
     iss.str(userInput);
-    iss >> numZombie;
+    iss >> numOfZombie_;
     iss.clear();
-    //std::cout << numLand << " " << numZombie << "\n";
-    /* Set Map */
+    if (numOfLand_ < 1 || numOfLand_ > 10) {numOfZombie_ = DEFAULT_ZOMBIE;}
+    if (numOfZombie_ < 1 || numOfZombie_ > 10) {numOfLand_ = DEFAULT_LAND;}
     std::cout
     << "=========================================================\n"
     << "Plants vs Zombies Rule:\n"
@@ -56,7 +96,45 @@ void Game::GameSetUp()
     << "  All plants are dead.\n"
     << "=============================================================================\n";
     system("Pause");
+    system("CLS");
+}
+void Game::Play()
+{
+    map_->Show();
+    std::cout << "---------------------------\n";
+    for (size_t i = 0; i < zombies_.size(); ++i) {
+        std::cout << *zombies_[i] << '\n';
+    }
+    std::cout << "===========================\n";
+    //waiting for plain virtual show()
+    // for (int i = 0; i < NUM_OF_PLANT_TYPES; ++i) {
+    //     std::cout << plants_[i] << "\n";
+    // }
+    printf("Player $%d:\t Enter Your choice (4 to give up, default : 4)...>", player_->Money());
+    int action = 0;
+    std::string userInput;
+    getline(std::cin, userInput);
+    std::istringstream iss(userInput);
+    iss >> action;
+    if (action < 1 || action > DEFAULT_ACTION) {action = DEFAULT_ACTION;}
+    if (action == DEFAULT_ACTION)
+    {
+        //checkGame
+        system("Pause");
+    }
+    else
+    {
+        //plant
+        system("Pause");
+    }
 }
 Game::~Game()
 {
+    delete player_;
+    for (size_t i = 0; i < zombies_.size(); ++i) {
+        delete zombies_[i];
+    }
+    for (size_t i = 0; i < NUM_OF_PLANT_TYPES; ++i) {
+        delete plants_[i];
+    }
 }
--- Game.h
+++ Game.h
@@ -1,14 +1,30 @@
 #ifndef GAME_H
 #define GAME_H
+#include "Game.h"
+#include "Player.h"
+#include "Zombie.h"
+#include "Map.h"
+#include "BombPlant.h"
+#include "CoinPlant.h"
+#include "HealPlant.h"
+#include "HornPlant.h"
+#include <vector>
 class Game
 {
 private:
     static constexpr char fileName[] = "plants.txt";
     void GameSetUp();
     bool success_;
+    static constexpr size_t NUM_OF_PLANT_TYPES = 4, DEFAULT_LAND = 8, DEFAULT_ZOMBIE = 3, DEFAULT_ACTION = 4;
 public:
     Game();
+    void Play();
     ~Game();
+    Plant *plants_[NUM_OF_PLANT_TYPES];
+    Player *player_;
+    std::vector<Zombie*> zombies_;
+    Map *map_;
+    size_t numOfLand_, numOfZombie_;
     bool Success() const {return success_;}
 };
 #endif
