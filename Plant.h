0c40d167ade7446760e38e001afe06bb3039af44
owancato <owancato@gmail.com>
2021-06-21 14:18:06 +0800

    fix the order of enum

--- Plant.h
+++ Plant.h
@@ -7,14 +7,14 @@
 constexpr int PLANT_NUMBER = 4;
 
 enum plantType{
-    HEAL_PLANT,
     COIN_PLANT,
-    BOMB_PLANT,
     HORN_PLANT,
+    BOMB_PLANT,
+    HEAL_PLANT,
     EMPTY
 };
 
-static std::string plantName[PLANT_NUMBER]={"Healflower","Mushroom","Bombflower","Hornflower"};
+static std::string plantName[PLANT_NUMBER]={"Mushroom","Hornflower","Bombflower","Healflower"};
 
 class Plant{
     private:
